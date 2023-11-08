a1738d7afa2705b166135509f7da15d41d6d5586
Kuyoshbek Mekhmonov <kuyoshbek_mekhmonov@epam.com>
2023-11-08 23:34:59 +0500

    task-7

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,8 +1,8 @@
 const API_PATHS = {
-  product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  product: "https://2c6q5f2vp9.execute-api.us-east-1.amazonaws.com",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://3av7mbfzi5.execute-api.us-east-1.amazonaws.com",
+  bff: "https://2c6q5f2vp9.execute-api.us-east-1.amazonaws.com",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
